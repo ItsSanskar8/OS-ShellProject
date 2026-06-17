51d67dd30093fcd85ac995ec5b2d70100c59f944
Sanskar Bhanderi <sanskarbhanderi08@gmail.com>
2026-06-17 12:16:43 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-        // System.out.print("$ ");
+    public static void main(String[] args) {
+
+        while (true) {
+
+            // Print shell prompt
+            System.out.print("$ ");
+            System.out.flush();
+
+            // Temporary infinite loop
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
     }
 }
