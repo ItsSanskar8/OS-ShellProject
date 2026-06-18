2b9a7295bba872d93dadbd99dcd38a80a5036b1a
Sanskar Bhanderi <sanskarbhanderi08@gmail.com>
2026-06-18 16:17:14 +0530

    codecrafters submit [skip ci]

--- .codecrafters/run.sh
+++ .codecrafters/run.sh
@@ -3,9 +3,16 @@ set -e
 
 stty -icanon -echo min 1 time 0 2>/dev/null || true
 
-java --enable-preview -cp target/classes Main
-status=$?
+MAIN_CLASS_PATH=$(find target/classes -name Main.class | head -n 1)
+
+if [ -n "$MAIN_CLASS_PATH" ]; then
+  MAIN_CLASS=$(printf "%s\n" "$MAIN_CLASS_PATH" | sed 's#^target/classes/##; s#/#.#g; s#\.class$##')
+  java --enable-preview -cp target/classes "$MAIN_CLASS"
+  status=$?
+  stty sane 2>/dev/null || true
+  exit $status
+fi
 
 stty sane 2>/dev/null || true
-
-exit $status
+echo "Main.class not found"
+exit 1
